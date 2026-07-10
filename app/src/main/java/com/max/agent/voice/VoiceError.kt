c76588c19f5ee51103a80feab21ef606d8f01922
thepotatoninjahost <thepotatoninjahost@gmail.com>
2026-07-10 15:33:12 -0700

    Delete app directory

--- app/src/main/java/com/max/agent/voice/VoiceError.kt
+++
@@ -1,8 +0,0 @@
-package com.max.agent.voice
-
-sealed class VoiceError {
-    data object MicBlocked : VoiceError()
-    data object NetworkRequired : VoiceError()
-    data object RecognizerBusy : VoiceError()
-    data class Unknown(val code: Int) : VoiceError()
-}
